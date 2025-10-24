8eb882926fbda7549f3a8da9d27bb0e288a5e4f3
mhmadkadoor <mhmadkadoor2004@gmail.com>
2025-10-24 23:17:11 +0300

    Add simulation script for energy anomaly detection scenario

---
+++ run.py
@@ -0,0 +1,7 @@
+from src.simulation import run_normal_flow, run_anomaly_flow, print_scenario_summary
+
+
+if __name__ == "__main__":
+    run_normal_flow()
+    run_anomaly_flow()
+    print_scenario_summary()
